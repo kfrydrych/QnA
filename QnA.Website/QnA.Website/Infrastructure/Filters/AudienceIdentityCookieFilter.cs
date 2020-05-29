8430ff393e829f8c19800ee88358a49d5d81a679
kfrydrych <36896890+kfrydrych@users.noreply.github.com>
2020-05-29 10:06:34 +0100

    Coockie action filter added.

---
+++ QnA.Website/QnA.Website/Infrastructure/Filters/AudienceIdentityCookieFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Threading.Tasks;
+
+namespace QnA.Website.Infrastructure.Filters
+{
+    public class AudienceIdentityCookieFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var storedCookie = context.HttpContext.Request.Cookies["audience-id"];
+
+            if (storedCookie == null)
+            {
+                var key = "audience-id";
+                var value = Guid.NewGuid().ToString();
+                var options = new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) };
+                context.HttpContext.Response.Cookies.Append(key, value, options);
+            }
+
+            await next();
+        }
+    }
+}
