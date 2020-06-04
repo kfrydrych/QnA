d72709e3f85c9015474f95befc9153f3e935688b
kfrydrych <36896890+kfrydrych@users.noreply.github.com>
2020-06-04 21:03:57 +0100

    Logs added to UpdateSessionsStatusHandler

--- QnA.Website/QnA.Application/Tasks/UpdateSessionsStatus/UpdateSessionsStatusHandler.cs
+++ QnA.Website/QnA.Application/Tasks/UpdateSessionsStatus/UpdateSessionsStatusHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using QnA.Application.Interfaces;
 using QnA.Domain.Interfaces;
 using QnA.Domain.Models;
@@ -13,12 +14,14 @@ namespace QnA.Application.Tasks.UpdateSessionsStatus
         private readonly IUnitOfWork _unitOfWork;
         private readonly IDateService _dateService;
         private readonly IUser _user;
+        private readonly ILogger _logger;
 
-        public UpdateSessionsStatusHandler(IUnitOfWork unitOfWork, IDateService dateService, IUser user)
+        public UpdateSessionsStatusHandler(IUnitOfWork unitOfWork, IDateService dateService, IUser user, ILogger logger)
         {
             _unitOfWork = unitOfWork;
             _dateService = dateService;
             _user = user;
+            _logger = logger;
         }
 
         public async Task<Unit> Handle(UpdateSessionsStatusCommand request, CancellationToken cancellationToken)
@@ -39,6 +42,11 @@ namespace QnA.Application.Tasks.UpdateSessionsStatus
             sessionsToBeArchived.ForEach(x => x.Archive(_user.Username, _dateService));
 
             await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            #region Logs
+            _logger.LogInformation($"{sessionsToBeOffline.Count} session(s) set to offline");
+            _logger.LogInformation($"{sessionsToBeArchived.Count} session(s) archived");
+            #endregion
 
             return Unit.Value;
         }
