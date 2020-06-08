e62691b211c1598321bb9c2711c8089766873c19
kfrydrych <36896890+kfrydrych@users.noreply.github.com>
2020-06-08 23:38:39 +0100

    audit table dev completed

---
+++ QnA.Website/QnA.Persistence/Migrations/20200608214705_AuditTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QnA.Persistence.Migrations
+{
+    public partial class AuditTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuditRecords",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TableName = table.Column<string>(maxLength: 100, nullable: false),
+                    EventMessage = table.Column<string>(maxLength: 150, nullable: false),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    SubjectId = table.Column<Guid>(nullable: false),
+                    KeyValues = table.Column<string>(nullable: true),
+                    OldValues = table.Column<string>(nullable: true),
+                    NewValues = table.Column<string>(nullable: true),
+                    ModifiedBy = table.Column<string>(maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditRecords", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditRecords");
+        }
+    }
+}
